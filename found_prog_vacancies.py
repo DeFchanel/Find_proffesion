7c6cda3a2083ed203b36f1a686dc37cc0bdce154
DeFchanel <georgyasr@mail.ru>
2023-03-10 19:32:25 +0300

    Made improvements

--- found_prog_vacancies.py
+++ found_prog_vacancies.py
@@ -6,30 +6,31 @@ from terminaltables import AsciiTable
 
 
 
-def predict_rub_salary_sj(lang):
+def get_sj_salary_statistics(lang, sj_key):
     sj_salaries = []
+    town_id = 4
     headers = {
         "X-Api-App-Id" : sj_key
     }
     for page in count(0):
         sj_payload = {
             'keyword': f'программист {lang}',
-            'town': 4,
+            'town': town_id,
             'page': page
         }
         superjob_response = requests.get('https://api.superjob.ru/2.0/vacancies/', headers=headers, params=sj_payload)
         superjob_response.raise_for_status()
         superjob_answer = superjob_response.json()
-        for vacancie in superjob_answer['objects']:
-            salary_from = vacancie['payment_from']
-            salary_to = vacancie['payment_to']
-            salary_currency = vacancie['currency']
+        vacancies_found = superjob_answer['total']
+        for vacancy in superjob_answer['objects']:
+            salary_from = vacancy['payment_from']
+            salary_to = vacancy['payment_to']
+            salary_currency = vacancy['currency']
             sj_salary = predict_rub_salary(salary_from, salary_to, salary_currency)
             if sj_salary:
                 sj_salaries.append(sj_salary)
         if not superjob_answer['more']:
             break
-    vacancies_found = superjob_answer['total']
     vacancies_processed = len(sj_salaries)
     if vacancies_processed:
         average_salary = sum(sj_salaries) // vacancies_processed
@@ -45,31 +46,32 @@ def predict_rub_salary_sj(lang):
 def predict_rub_salary(salary_from, salary_to, salary_currency):
     if salary_currency != 'RUR' and salary_currency != 'rub':
         return None
-    if not salary_from:
-        return (salary_to * 0.8)
-    elif not salary_to:
-        return (salary_from * 1.2)
-    else:
-        return ((salary_from + salary_to) / 2)
+    if salary_from and salary_to:
+        return (salary_from + salary_to) / 2
+    elif salary_to:
+        return salary_to * 0.8
+    elif salary_from:
+        return salary_from * 1.2
 
 
-def predict_rub_salary_hh(lang):
+def get_hh_salary_statistics(lang):
     hh_salaries = []
+    area_id = 1
     for page in count(0):
         payload = {
             'text': f'программист {lang}',
-            'area': 1,
+            'area': area_id,
             'page': page
         }
         response_hh = requests.get('https://api.hh.ru/vacancies', params=payload)
         response_hh.raise_for_status()
         response_hh_payload = response_hh.json()
-        for vacancie in response_hh_payload['items']:
-            vacancie_salary = vacancie['salary']
-            if vacancie_salary:
-                salary_from = vacancie_salary['from']
-                salary_to = vacancie_salary['to']
-                salary_currency = vacancie_salary['currency']
+        for vacancy in response_hh_payload['items']:
+            vacancy_salary = vacancy['salary']
+            if vacancy_salary:
+                salary_from = vacancy_salary['from']
+                salary_to = vacancy_salary['to']
+                salary_currency = vacancy_salary['currency']
                 salary = predict_rub_salary(salary_from, salary_to, salary_currency)
                 if salary:
                     hh_salaries.append(salary)
@@ -90,14 +92,14 @@ def predict_rub_salary_hh(lang):
 
 
 def make_table(lang_salaries, title):
-    table_data = [
+    table_content = [
         ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']
     ]
-    for lang, vacancie_info in lang_salaries.items():
-        raw = [lang, vacancie_info['vacancies_found'], vacancie_info['vacancies_processed'], vacancie_info['average_salary']]
-        table_data.append(raw)
-    table = AsciiTable(table_data, title)
-    print(table.table)
+    for lang, vacancy in lang_salaries.items():
+        raw = [lang, vacancy['vacancies_found'], vacancy['vacancies_processed'], vacancy['average_salary']]
+        table_content.append(raw)
+    table = AsciiTable(table_content, title)
+    return table.table
 
 
 if __name__ == '__main__':
@@ -108,14 +110,16 @@ if __name__ == '__main__':
         'Java',
         'Python'
     ]
-    lang_salaries_hh = {}
-    lang_salaries_sj = {}
+    lang_salaries_statistics_hh = {}
+    lang_salaries_statistics_sj = {}
     title_sj = 'SuperJob Moscow'
     title_hh = 'HeadHunter Moscow'
     for lang in prog_lang:
-        lang_salary_sj = predict_rub_salary_sj(lang)
-        lang_salaries_sj[lang] = lang_salary_sj
-        lang_salary_hh = predict_rub_salary_hh(lang)
-        lang_salaries_hh[lang] = lang_salary_hh
-    make_table(lang_salaries_sj, title_sj)
-    make_table(lang_salaries_hh, title_hh)
+        lang_salary_statistics_sj = get_sj_salary_statistics(lang, sj_key)
+        lang_salaries_statistics_sj[lang] = lang_salary_statistics_sj
+        lang_salary_statistics_hh = get_hh_salary_statistics(lang)
+        lang_salaries_statistics_hh[lang] = lang_salary_statistics_hh
+    table_sj = make_table(lang_salaries_statistics_sj, title_sj)
+    table_hh = make_table(lang_salaries_statistics_hh, title_hh)
+    print(table_sj)
+    print(table_hh)
